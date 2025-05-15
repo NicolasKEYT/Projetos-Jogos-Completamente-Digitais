18d62daef6015d17a9bf1ad8bdc7bc6191043f4d
Nicolas Keyt <142526482+NicolasKEYT@users.noreply.github.com>
2025-05-14 21:08:48 -0300

    Delete TanquesDaTerra-Layers directory

--- TanquesDaTerra-Layers/camera.py
+++
@@ -1,9 +0,0 @@
-class Camera:
-    def __init__(self, level_width, level_height):
-        self.offset = 0
-        self.level_width = level_width
-        self.level_height = level_height
-
-    def update(self, target_rect):
-        x = target_rect.centerx - 800//2
-        self.offset = max(0, min(x, self.level_width - 800))
